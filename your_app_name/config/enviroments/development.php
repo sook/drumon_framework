6fad96be768f806a90a534fd7dd711d1ac8015db
Danillo César <danillos@gmail.com>
2010-10-22 15:02:46 -0300

    Fix no nome do css

--- your_app_name/config/enviroments/development.php
+++ your_app_name/config/enviroments/development.php
@@ -18,7 +18,7 @@
 	
 	
 	// Application Paths
-	define('STYLESHEETS_PATH', APP_DOMAIN.'/public/css/');
+	define('STYLESHEETS_PATH', APP_DOMAIN.'/public/stylesheets/');
 	define('JAVASCRIPTS_PATH',APP_DOMAIN.'/public/javascripts/');
 	define('IMAGES_PATH',APP_DOMAIN.'/public/images/');
 	define('MODULES_PATH', APP_DOMAIN.'/public/modules/');
