b92d940e13c58a035ffe2e20080b6238e7648f3d
Danillo César <danillos@gmail.com>
2011-03-26 10:28:31 -0300

    Alterado home para pages controller.
    Faz mais sentido ter o pages no lugar do home.

--- your_app_name/app/controllers/pages_controller.php
+++ your_app_name/app/controllers/pages_controller.php
@@ -1,11 +1,12 @@
 <?php
 
-	class HomeController extends AppController {
-	
+	class PagesController extends AppController {
+		
 		function index() {
 			// Gera chave para o app_secret
 			$this->add('app_secret', md5(uniqid()));
 		}
+		
 	}
 	
 ?>
