19194a665b11966e4623fe948f4919b0580ecc91
Danillo César <danillos@gmail.com>
2011-04-13 21:42:56 -0300

    Método to_sql() agora mostra o SQL já com os paramentos.
    Antes:
    SELECT * FROM news WHERE id = ?
    
    Depois:
    SELECT * FROM news WHERE id = 1

--- your_app_name/vendor/plugins/drumon_model/class/drumon_model.php
+++ your_app_name/vendor/plugins/drumon_model/class/drumon_model.php
@@ -728,7 +728,33 @@
 		 * @return string
 		 */
 		public function to_sql() {
-			return $this->generate_sql();
+			return $this->interpolateQuery($this->generate_sql(), $this->__statements);
+		}
+		
+		/**
+		 * Replaces any parameter placeholders in a query with the value of that
+		 * parameter. Useful for debugging. Assumes anonymous parameters from 
+		 * $params are are in the same order as specified in $query
+		 *
+		 * @param string $query The sql query with parameter placeholders
+		 * @param array $params The array of substitution parameters
+		 * @return string The interpolated query
+		 *
+		 * @author http://stackoverflow.com/questions/210564/pdo-prepared-statements/1376838#1376838
+		 */
+		public static function interpolateQuery($query, $params) {
+			$keys = array();
+
+			# build a regular expression for each parameter
+			foreach ($params as $key => $value) {
+				if (is_string($key)) {
+					$keys[] = '/:'.$key.'/';
+				} else {
+					$keys[] = '/[?]/';
+				}
+			}
+			$query = preg_replace($keys, $params, $query, 1, $count);
+			return $query;
 		}
 		
 		/**
